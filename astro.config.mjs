c2709167e60d0f13ce46e96dbf97d172a8f73f64
josue.ga00 <josue.ga00@gmail.com>
2024-09-22 07:49:39 +0000

    Eliminado i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [tailwind()],
   site: 'https://josuejx.github.io',
   base: '',
 });
